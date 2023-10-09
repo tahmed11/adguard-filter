71b41a0211cb7fea07b87c0759af1ef3a1e2f03b
tahmed11 <tanvir.ahmed@outlook.com>
2023-10-09 14:49:33 +1100

    test workflow

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt -O filter.txt
 cat filter.txt | grep '^||' | sed 's/\^//' | sed 's/\|//g' | grep -v '*'>> hosts.txt
-while read line; do echo "0.0.0.0" $line >> adguard_filtered.txt; done <hosts.txt
+while read line; do echo "0.0.0.0" $line >> adguard_hosts.txt; done <hosts.txt
