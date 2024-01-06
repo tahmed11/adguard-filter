658e3900422a1732923bd5fa78d4426a7fe93d8c
tahmed11 <tanvir.ahmed@outlook.com>
2024-01-06 23:29:18 +1100

    update

--- generate.sh
+++ generate.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm adguard_hosts.txt
 wget https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt -O filter.txt
-cat filter.txt | grep '^||' | sed 's/\^//' | sed 's/\|//g' | grep -v '*'>> hosts.txt
+cat filter.txt | grep '^||' | sed 's/\^//' | sed 's/\|//g' | grep -v '*'> hosts.txt
 while read line; do echo "0.0.0.0" $line >> adguard_hosts.txt; done <hosts.txt
