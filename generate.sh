e3d29730b254651f787140234bbd74e4a6d0e27f
tahmed <25324534+tahmed11@users.noreply.github.com>
2025-02-18 15:02:36 +1100

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -3,3 +3,5 @@ rm adguard_hosts.txt
 wget https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt -O filter.txt
 cat filter.txt | grep '^||' | sed 's/\^//' | sed 's/|//g' | grep -v '*'> hosts.txt
 while read line; do echo "0.0.0.0" $line >> adguard_hosts.txt; done <hosts.txt
+# add custom tlds
+while read line; do echo $line >> adguard_hosts.txt; done < blocklists.txt
